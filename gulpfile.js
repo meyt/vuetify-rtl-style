9b3fdde379b00bcddf89a157ba5d979af7c55764
meyt <pasd3000@gmail.com>
2018-05-24 12:01:29 +0430

    Disable sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,13 @@
 var gulp = require('gulp')
 var sass = require('gulp-sass')
 var cleanCSS = require('gulp-clean-css')
-var sourcemaps = require('gulp-sourcemaps')
+// var sourcemaps = require('gulp-sourcemaps')
 var autoprefixer = require('gulp-autoprefixer')
 var rename = require('gulp-rename')
 
 gulp.task('workflow', function () {
   gulp.src('./src/scss/**/*.scss')
-    .pipe(sourcemaps.init())
+    // .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer({
       browsers: ['last 2 versions'],
@@ -17,7 +17,7 @@ gulp.task('workflow', function () {
     }))
     .pipe(gulp.dest('./dist/css/')) // unminified
     .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(sourcemaps.write('./'))
+    // .pipe(sourcemaps.write('./'))
     .pipe(rename(function (path) {
       if (path.extname === '.css') {
         path.basename += '.min'
